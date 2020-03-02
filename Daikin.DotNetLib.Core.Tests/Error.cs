7efedc00d4b38a850764ffd5f296277f4750902d
Hans Dickel <hans@recursivegeek.com>
2020-03-02 09:07:00 -0600

    Add support for formatted error messaging (for consistency across applications).

---
+++ Daikin.DotNetLib.Core.Tests/Error.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace Daikin.DotNetLib.Core.Tests
+{
+    public class Error
+    {
+        [Fact]
+        public void BuildDate()
+        {
+            var when = new DateTime(2019,3,1,8,46,57);
+            var errorMessage = Data.Error.BuildDate(when);
+            Assert.Equal("1903010846", errorMessage);
+        }
+
+        [Fact]
+        public void BuildString()
+        {
+            var errorMessage = Data.Error.BuildString("Ref A17", "Unable to access the whatchamacallit", errorNumber: 27);
+            Assert.Equal("Error Encountered: Unable to access the whatchamacallit (Ref A17 #27)", errorMessage);
+        }
+
+    }
+}
---
+++ Daikin.DotNetLib.Data/Error.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Daikin.DotNetLib.Data
+{
+    public enum ErrorType
+    {
+        Error,
+        Warning,
+        Informational
+    }
+
+    public static class Error
+    {
+        public static string BuildString(string code, string message, ErrorType errorType = ErrorType.Error, int? errorNumber = null, DateTime? when = null)
+        {
+            string errorTypeMsg;
+            switch (errorType)
+            {
+                case ErrorType.Error:
+                    errorTypeMsg = "Error Encountered";
+                    break;
+                case ErrorType.Warning:
+                    errorTypeMsg = "Warning Notification";
+                    break;
+                case ErrorType.Informational:
+                    errorTypeMsg = "Informational Notification";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(errorType), errorType, null);
+            }
+
+            var errorId = (errorNumber == null ? BuildDate(when ?? DateTime.Now) : "#" + errorNumber);
+            return $"{errorTypeMsg}: {message} ({code} {errorId})";
+        }
+
+        public static string BuildDate(DateTime when)
+        {
+            return $"{when.Year.ToString().Substring(2)}{PadNumber(when.Month)}{PadNumber(when.Day)}{PadNumber(when.Hour)}{PadNumber(when.Minute)}";
+        }
+
+        private static string PadNumber(int number)
+        {
+            return number.ToString().PadLeft(2, '0');
+        }
+    }
+}
