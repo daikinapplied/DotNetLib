7bfabf0847b146e60a1e91fd7610bf928861df39
Hans Dickel <hans@recursivegeek.com>
2021-02-11 19:42:32 -0500

    Add WriteTo.DetailedMsSql() support

--- Daikin.DotNetLib.Serilog/MsSqlServer.cs
+++ Daikin.DotNetLib.Serilog/MsSqlServer.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using Serilog.Configuration;
 using Serilog.Exceptions;
 using Serilog.Sinks.MSSqlServer;
 
@@ -13,16 +14,22 @@ namespace Daikin.DotNetLib.Serilog
     public static class MsSqlServer
     {
         /// <summary>
-        /// Add MSSQL Server to logger configuration with pre-defined custom columns
+        /// Adds WriteTo.DetailedMsSql() MSSQL Server to logger configuration with pre-defined custom columns
         /// </summary>
-        /// <param name="loggerConfiguration">Chaining of LoggerConfiguration</param>
+        /// <param name="loggerSinkConfiguration">The logger configuration</param>
         /// <param name="caller">typeof(Startup) or typeof(Program) will likely work</param>
         /// <param name="configuration">(Optional) Read appsettings configuration</param>
         /// <param name="sqlConnectionStr">(Optional) SQL Connection string - if not available, attempt to get from ConnectionStrings:DefaultConnection in the configuration</param>
-        /// <param name="tableName">(Optional) Table name for Serilog</param>
         /// <param name="env">(Optional) Environment information - if not available, attempt to get Environmental Variable ASPNETCORE_ENVIRONMENT</param>
+        /// <param name="sinkOptions">Supples additional settings for the sink</param>
         /// <returns></returns>
-        public static LoggerConfiguration Add(this LoggerConfiguration loggerConfiguration, Type caller, IConfiguration configuration = null, string sqlConnectionStr = null, string tableName = "!_Serilog", IHostingEnvironment env = null)
+        public static LoggerConfiguration DetailedMsSql(
+            this LoggerSinkConfiguration loggerSinkConfiguration,
+            Type caller, 
+            IConfiguration configuration = null, 
+            string sqlConnectionStr = null, 
+            IHostingEnvironment env = null,
+            MSSqlServerSinkOptions sinkOptions = null)
         {
             var appName = caller.GetTypeInfo().Assembly.GetName();
             var applicationName = appName.Name.Truncate(Constants.MaxLengthApplication);
@@ -35,47 +42,42 @@ namespace Daikin.DotNetLib.Serilog
             var columnOptions = new ColumnOptions
             {
                 AdditionalColumns = new List<SqlColumn>
-                        {
-                            new SqlColumn { ColumnName = "EventId", DataType = SqlDbType.Int, AllowNull = true },
-                            new SqlColumn { ColumnName = "Application", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthApplication, AllowNull = true },
-                            new SqlColumn { ColumnName = "Version", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthVersion, AllowNull = true },
-                            new SqlColumn { ColumnName = "RemoteIp", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthRemoteIp, AllowNull = true },
-                            new SqlColumn { ColumnName = "ClientId", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthClientId, AllowNull = true },
-                            new SqlColumn { ColumnName = "Environment", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthEnvironment, AllowNull = true },
-                            new SqlColumn { ColumnName = "User", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthUser, AllowNull = true },
-                            new SqlColumn { ColumnName = "Source", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthSource, AllowNull = true },
-                            new SqlColumn { ColumnName = "Session", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthSession, AllowNull = true },
-                            new SqlColumn { ColumnName = "UserAgent", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthUserAgent, AllowNull = true },
-                            new SqlColumn { ColumnName = "RequestId", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthRequestId, AllowNull = true },
-                            new SqlColumn { ColumnName = "Data", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthData, AllowNull = true }
-                        }
+                    {
+                        new SqlColumn { ColumnName = "EventId", DataType = SqlDbType.Int, AllowNull = true },
+                        new SqlColumn { ColumnName = "Application", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthApplication, AllowNull = true },
+                        new SqlColumn { ColumnName = "Version", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthVersion, AllowNull = true },
+                        new SqlColumn { ColumnName = "RemoteIp", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthRemoteIp, AllowNull = true },
+                        new SqlColumn { ColumnName = "ClientId", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthClientId, AllowNull = true },
+                        new SqlColumn { ColumnName = "Environment", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthEnvironment, AllowNull = true },
+                        new SqlColumn { ColumnName = "User", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthUser, AllowNull = true },
+                        new SqlColumn { ColumnName = "Source", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthSource, AllowNull = true },
+                        new SqlColumn { ColumnName = "Session", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthSession, AllowNull = true },
+                        new SqlColumn { ColumnName = "UserAgent", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthUserAgent, AllowNull = true },
+                        new SqlColumn { ColumnName = "RequestId", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthRequestId, AllowNull = true },
+                        new SqlColumn { ColumnName = "Data", DataType = SqlDbType.NVarChar, DataLength = Constants.MaxLengthData, AllowNull = true }
+                    }
             };
             columnOptions.Store.Add(StandardColumn.LogEvent); // want JSON data
             columnOptions.Store.Remove(StandardColumn.Properties); // don't need XML data
 
-            if (loggerConfiguration == null) loggerConfiguration = new LoggerConfiguration();
+            if (sinkOptions == null)
+            {
+                sinkOptions = new MSSqlServerSinkOptions { AutoCreateSqlTable = true, TableName = "!_Serilog" };
 
-            loggerConfiguration
-                //.WriteTo.Console(outputTemplate: Theme.Template, theme: Theme.System)
-                .WriteTo.MSSqlServer(
-                    connectionString: sqlConnectionStr,
-                    sinkOptions: new MSSqlServerSinkOptions
-                    {
-                        AutoCreateSqlTable = true,
-                        TableName = tableName,
-                    },
-                    columnOptions: columnOptions
-                )
+            }
+
+            var loggerConfiguration = loggerSinkConfiguration.MSSqlServer(
+                connectionString: sqlConnectionStr,
+                appConfiguration: configuration,
+                columnOptions: columnOptions,
+                sinkOptions: sinkOptions);
+
+            return loggerConfiguration
                 .Enrich.FromLogContext()
                 .Enrich.WithExceptionDetails()
                 .Enrich.WithProperty("Application", applicationName)
                 .Enrich.WithProperty("Version", applicationVersion)
                 .Enrich.WithProperty("Environment", environment);
-
-            if (configuration != null) loggerConfiguration.ReadFrom.Configuration(configuration); // get additional settings from appsettings.json
-
-            return loggerConfiguration;
         }
-
     }
 }
