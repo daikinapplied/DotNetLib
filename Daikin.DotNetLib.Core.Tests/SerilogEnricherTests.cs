1ebb1d93464e03e3999c1ed9e6ea82692f0416dc
Hans Dickel <hans@recursivegeek.com>
2021-02-15 20:47:22 -0500

    Add Tests

---
+++ Daikin.DotNetLib.Core.Tests/SerilogEnricherTests.cs
@@ -0,0 +1,20 @@
+﻿using Daikin.DotNetLib.Serilog;
+using Xunit;
+
+namespace Daikin.DotNetLib.Core.Tests
+{
+    public class SerilogEnricherTests
+    {
+        [Fact]
+        public void TestTruncate()
+        {
+            string s = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Null(s.Truncate(5));
+            s = "1234567890";
+            Assert.Equal("12345", s.Truncate(5));
+            s = "12";
+            Assert.Equal("12", s.Truncate(10));
+        }
+    }
+}
